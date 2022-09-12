9cc7cd5cf6ed4a33b1ca7bdc9ac75cc2e29a45c4
b17000 <90886946+kurumicute@users.noreply.github.com>
2022-09-12 20:44:11 +0800

    Add files via upload

---
+++ slot.js
@@ -0,0 +1,44 @@
+
+function fisherYatesShuffle(arr){
+    for(var i =arr.length-1 ; i>0 ;i--){
+        var j = Math.floor( Math.random() * (i + 1) );
+        [arr[i],arr[j]]=[arr[j],arr[i]];
+    }
+}
+
+document.getElementById("d3").onclick = function(){
+    var user = ['第1個','第2個','第3個','第4個'];
+    var name = document.getElementById("t1")
+    var a = /^[a-zA-Z()]+$/.test(name.value);
+    var b = /^[1-9()]+$/.test(name.value);
+    var c = /^[1-9a-zA-Z()]+$/.test(name.value);
+    var e = /[^\u4e00-\u9fa51-9]+$/.test(name.value);
+    var g = /[^\u4e00-\u9fa5a-zA-Z]+$/.test(name.value);
+    var i = /[^\u4e00-\u9fa5a-zA-Z1-9]+$/.test(name.value);
+    fisherYatesShuffle(user);
+    document.getElementById("d4").innerHTML = user;
+    if (name.value.length == 0){
+        document.getElementById("d4").innerHTML ="沒有輸入名字";
+    }
+    else if (a == true) {
+        document.getElementById("d4").innerHTML = "我不信你名字是用英文";
+    }
+    else if (b == true) {
+        document.getElementById("d4").innerHTML = "真•數字人";
+    }
+    else if (c == true){
+        document.getElementById("d4").innerHTML = "我不覺得英文和數字可以組出名字";
+    }
+    else if (e == true){
+        document.getElementById("d4").innerHTML = "英文和中文組合不會比較帥";
+    }
+    else if (g == true){
+        document.getElementById("d4").innerHTML = "ok,數字人是吧";
+    }
+    else if (i == true){
+        document.getElementById("d4").innerHTML = "我全都要";
+    }
+    else{
+        document.getElementById("d4").innerHTML = user;
+    }
+    }
